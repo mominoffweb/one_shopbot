d290ae75cd3d2c19f6ea9b5c2a0b92ee0aee01bf
mominoffweb <mjamshidbek@gmail.com>
2025-03-15 18:11:42 +0500

    one_shopbot

---
+++ app.js/app.js
@@ -0,0 +1,14 @@
+const btn_phone = document.querySelector(".phone-icon");
+
+const btn_lang = document.querySelector();
+
+btn_phone.addEventListener;
+
+function toggleContacts() {
+  let contactBox = document.getElementById("navbar");
+  contactBox.classList.toggle("hiddin");
+}
+
+document.getElementById("phoneIcon").addEventListener("click", function () {
+  window.location.href = "contact.html"; 
+});
