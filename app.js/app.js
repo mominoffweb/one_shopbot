37bd1e860be1cb15d692972bb743ef7c10015bb7
mominoffweb <mjamshidbek@gmail.com>
2025-03-18 11:54:13 +0500

    oneshop_bot

--- app.js/app.js
+++ app.js/app.js
@@ -1,14 +1,22 @@
-const btn_phone = document.querySelector(".phone-icon");
+// const btn_phone = document.querySelector(".phone-icon");
 
-const btn_lang = document.querySelector();
+// const btn_lang = document.querySelector();
 
-btn_phone.addEventListener;
+// btn_phone.addEventListener;
 
-function toggleContacts() {
-  let contactBox = document.getElementById("navbar");
-  contactBox.classList.toggle("hiddin");
-}
+// function toggleContacts() {
+//   let contactBox = document.getElementById("navbar");
+//   contactBox.classList.toggle("hiddin");
+// }
 
-document.getElementById("phoneIcon").addEventListener("click", function () {
-  window.location.href = "contact.html"; 
+// document.getElementById("phoneIcon").addEventListener("click", function () {
+//   window.location.href = "contact.html";
+// });
+
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 2000, // Har 2 soniyada o‘tadi
+    disableOnInteraction: false, // Foydalanuvchi bosganidan keyin ham davom etsin
+  },
+  loop: true, // Cheksiz aylanish
 });
